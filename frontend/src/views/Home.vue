3ac2f1e65cf93ee62b23174dd4119b1e7ddd88e0
KangMin Her <81793748+nicecarrot@users.noreply.github.com>
2022-09-29 16:57:10 +0900

    NOT-15 [Client] Electron command: npm run electron:serve

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,141 +1,154 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="8">
-            <v-card class="elevation-12">
-              <v-window v-model="step">
-                <v-window-item :value="1">
-                  <v-row>
-                    <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
-                        <h1
-                          class="text-center display-2 teal--text text--accent-3"
-                        >Sign in to Noting</h1>
-                        <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
-                          </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-facebook-f</v-icon>
-                          </v-btn>
-                        </div>
-                        <h4 class="text-center mt-4">다른 소셜 계정으로 계속하기</h4>
-                        <v-form>
-                          <v-text-field
-                            label="Email"
-                            name="Email"
-                            prepend-icon="email"
-                            type="text"
-                            color="teal accent-3"
-                          />
+  <div class="main">
+    <v-col cols="12" sm="8" md="8">
+      <v-card class="elevation-12">
+        <v-window v-model="step">
+          <v-window-item :value="1">
+            <v-row>
+              <v-col cols="12" md="8">
+                <v-card-text class="mt-12">
+                  <h1 class="text-center display-2 teal--text text--lighten-1">
+                    Sign in to Noting
+                  </h1>
+                  <div class="text-center mt-4">
+                    <v-btn class="mx-2" fab color="black" outlined>
+                      <v-icon>fab fa-google-plus-g</v-icon>
+                    </v-btn>
+                    <v-btn class="mx-2" fab color="black" outlined>
+                      <v-icon>fab fa-facebook-f</v-icon>
+                    </v-btn>
+                  </div>
+                  <h4 class="text-center mt-4">
+                    다른 소셜 계정으로 계속하기
+                  </h4>
+                  <v-form>
+                    <v-text-field
+                      label="Email"
+                      name="Email"
+                      prepend-icon="email"
+                      type="text"
+                      color="teal lighten-1"
+                    />
 
-                          <v-text-field
-                            id="password"
-                            label="Password"
-                            name="password"
-                            prepend-icon="lock"
-                            type="password"
-                            color="teal accent-3"
-                          />
-                        </v-form>
-                        <h3 class="text-center mt-4">비밀번호를 잊으셨나요 ?</h3>
-                      </v-card-text>
-                      <div class="text-center mt-3">
-                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
-                      </div>
-                    </v-col>
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">오셨군요!</h1>
-                        <h5
-                          class="text-center"
-                        >함께 손쉬운 웨이팅이 가능한 곳으로 떠나기</h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-window-item>
-                <v-window-item :value="2">
-                  <v-row class="fill-height">
-                    <v-col cols="12" md="4" class="teal accent-3">
-                      <v-card-text class="white--text mt-12">
-                        <h1 class="text-center display-1">환영합니다!</h1>
-                        <h5
-                          class="text-center"
-                        >계속하시려면 정보를 입력해주세요</h5>
-                      </v-card-text>
-                      <div class="text-center">
-                        <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
-                      </div>
-                    </v-col>
+                    <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      prepend-icon="lock"
+                      type="password"
+                      color="teal lighten-1"
+                    />
+                  </v-form>
+                  <h3 class="text-center mt-4">
+                    비밀번호를 잊으셨나요 ?
+                  </h3>
+                </v-card-text>
+                <div class="text-center mt-3">
+                  <v-btn rounded color="teal lighten-1" dark onclick=""
+                    >SIGN IN</v-btn
+                  >
+                </div>
+              </v-col>
+              <v-col cols="12" md="4" class="teal lighten-1">
+                <v-card-text class="white--text mt-12">
+                  <h1 class="text-center display-1">오셨군요!</h1>
+                  <h5 class="text-center">
+                    함께 손쉬운 웨이팅이 가능한 곳으로 떠나기
+                  </h5>
+                </v-card-text>
+                <div class="text-center">
+                  <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
+                </div>
+              </v-col>
+            </v-row>
+          </v-window-item>
+          <v-window-item :value="2">
+            <v-row class="fill-height">
+              <v-col cols="12" md="4" class="teal lighten-1">
+                <v-card-text class="white--text mt-12">
+                  <h1 class="text-center display-1">환영합니다!</h1>
+                  <h5 class="text-center">
+                    계속하시려면 정보를 입력해주세요
+                  </h5>
+                </v-card-text>
+                <div class="text-center">
+                  <v-btn rounded outlined dark @click="step--">Sign in</v-btn>
+                </div>
+              </v-col>
 
-                    <v-col cols="12" md="8">
-                      <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
-                        <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
-                          </v-btn>
-                          
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-facebook-f</v-icon>
-                          </v-btn>
+              <v-col cols="12" md="8">
+                <v-card-text class="mt-12">
+                  <h1 class="text-center display-2 teal--text text--lighten-1">
+                    Create Account
+                  </h1>
+                  <div class="text-center mt-4">
+                    <v-btn class="mx-2" fab color="black" outlined>
+                      <v-icon>fab fa-google-plus-g</v-icon>
+                    </v-btn>
 
-                          
-                        </div>
-                        <h4 class="text-center mt-4">다른 소셜 계정으로 계속하기</h4>
-                        <v-form>
-                          <v-text-field
-                            label="Name"
-                            name="Name"
-                            prepend-icon="person"
-                            type="text"
-                            color="teal accent-3"
-                          />
-                          <v-text-field
-                            label="Email"
-                            name="Email"
-                            prepend-icon="email"
-                            type="text"
-                            color="teal accent-3"
-                          />
+                    <v-btn class="mx-2" fab color="black" outlined>
+                      <v-icon>fab fa-facebook-f</v-icon>
+                    </v-btn>
+                  </div>
+                  <h4 class="text-center mt-4">
+                    다른 소셜 계정으로 계속하기
+                  </h4>
+                  <v-form>
+                    <v-text-field
+                      label="Name"
+                      name="Name"
+                      prepend-icon="person"
+                      type="text"
+                      color="teal lighten-1"
+                    />
+                    <v-text-field
+                      label="Email"
+                      name="Email"
+                      prepend-icon="email"
+                      type="text"
+                      color="teal lighten-1"
+                    />
 
-                          <v-text-field
-                            id="password"
-                            label="Password"
-                            name="password"
-                            prepend-icon="lock"
-                            type="password"
-                            color="teal accent-3"
-                          />
-                        </v-form>
-                      </v-card-text>
-                      <div class="text-center mt-n5">
-                        <v-btn rounded color="teal accent-3" dark>SIGN UP</v-btn>
-                      </div>
-                    </v-col>
-                  </v-row>
-                </v-window-item>
-              </v-window>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
-  </v-app>
+                    <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      prepend-icon="lock"
+                      type="password"
+                      color="teal lighten-1"
+                    />
+                  </v-form>
+                </v-card-text>
+                <div class="text-center mt-n5">
+                  <v-btn rounded color="teal lighten-1" dark>SIGN UP</v-btn>
+                </div>
+              </v-col>
+            </v-row>
+          </v-window-item>
+        </v-window>
+      </v-card>
+    </v-col>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    step: 1
+    step: 1,
   }),
   props: {
-    source: String
-  }
+    source: String,
+  },
 };
 </script>
+
+<style>
+.main {
+  display: flex;
+  width: 146vw;
+  padding-left: 1.2em;
+}
+::-webkit-scrollbar {
+    display: none;
+}
+</style>
